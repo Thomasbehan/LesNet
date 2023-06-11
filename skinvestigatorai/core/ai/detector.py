d8b85b1cfcf206cad7ea3b3a2bc77223cd3945d1
Thomas Behan <thomasbehan4@gmail.com>
2023-06-11 19:33:58 +0100

    M-1.5 - :recycle: Updated docker-compose.yml version to 0.1.5 and refactored SkinCancerDetector class for improved readability and modularity

--- skinvestigatorai/core/ai/detector.py
+++ skinvestigatorai/core/ai/detector.py
@@ -10,15 +10,19 @@ from vit_keras import vit, utils
 
 
 class SkinCancerDetector:
-    def __init__(self, train_dir, val_dir, test_dir, log_dir='logs'):
+    def __init__(self, train_dir, val_dir, test_dir, log_dir='logs', batch_size=128, model_dir='models/v1.5',
+                 img_size=(160, 160)):
         self.train_dir = train_dir
         self.val_dir = val_dir
         self.test_dir = test_dir
         self.log_dir = log_dir
+        self.batch_size = batch_size
+        self.img_size = img_size
+        self.model_dir = model_dir
         self.model = None
-        self.batch_size = 64
 
     def preprocess_data(self):
+        """Preprocess data and apply image augmentation."""
         aug = A.Compose([
             A.Rotate(limit=40),
             A.ShiftScaleRotate(shift_limit=0.2, scale_limit=0.2),
@@ -30,25 +34,32 @@ class SkinCancerDetector:
         train_generator = DataGen(
             self.train_dir,
             batch_size=self.batch_size,
-            img_height=160,
-            img_width=160,
+            img_height=self.img_size[0],
+            img_width=self.img_size[1],
             augmentations=aug)
 
-        val_datagen = ImageDataGenerator(rescale=1. / 255)
-        val_generator = val_datagen.flow_from_directory(
-            self.val_dir,
-            target_size=(160, 160),
-            batch_size=self.batch_size,
-            class_mode='categorical')
-
-        test_datagen = ImageDataGenerator(rescale=1. / 255)
+        val_generator = self.create_data_generator(self.val_dir)
+        test_datagen = self.create_data_generator()
 
         return train_generator, val_generator, test_datagen
 
+    def create_data_generator(self, dir=None):
+        """Create ImageDataGenerator."""
+        datagen = ImageDataGenerator(rescale=1. / 255)
+
+        if dir:
+            return datagen.flow_from_directory(
+                dir,
+                target_size=self.img_size,
+                batch_size=self.batch_size,
+                class_mode='categorical')
+
+        return datagen
+
     def build_model(self, num_classes):
-        image_size = 160
+        """Build the ViT model."""
         vit_model = vit.vit_b32(
-            image_size=image_size,
+            image_size=self.img_size[0],
             activation='softmax',
             pretrained=True,
             include_top=False,
@@ -71,40 +82,46 @@ class SkinCancerDetector:
 
         self.model = model
 
-    def train_model(self, train_generator, val_generator, epochs=3000):
-        if self.model is None:
-            raise ValueError("Model has not been built. Call build_model() first.")
+    def train_model(self, train_generator, val_generator, epochs=3000, patience_lr=160, patience_es=50, min_lr=1e-6,
+                    min_delta=1e-4):
+        """Train the model with callbacks."""
+        self._check_model()
 
         # Create a log directory with a timestamp
         current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         log_dir = os.path.join(self.log_dir, current_time)
         os.makedirs(log_dir, exist_ok=True)
 
-        # Set up the TensorBoard callback
-        tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True, write_images=True,
-                                           update_freq='epoch', profile_batch=0)
-        reduce_lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=160, min_lr=1e-6,
-                                               min_delta=1e-4)
-        model_checkpoint_callback = ModelCheckpoint(
-            filepath=os.path.join(log_dir, "{}_best_model.h5".format(current_time)),
-            save_best_only=True, monitor='val_loss', mode='min', verbose=1)
-        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=50, restore_best_weights=True)
+        callbacks = self._create_callbacks(log_dir, current_time, patience_lr, min_lr, min_delta, patience_es)
 
         history = self.model.fit(
             train_generator,
             epochs=epochs,
             validation_data=val_generator,
-            callbacks=[tensorboard_callback, reduce_lr_callback, model_checkpoint_callback, early_stopping_callback])
+            callbacks=callbacks)
         return history
 
+    def _create_callbacks(self, log_dir, current_time, patience_lr, min_lr, min_delta, patience_es):
+        """Create Keras callbacks."""
+        tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True, write_images=True,
+                                           update_freq='epoch', profile_batch=0)
+        reduce_lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.1, patience=patience_lr, min_lr=min_lr,
+                                               min_delta=min_delta)
+        model_checkpoint_callback = ModelCheckpoint(
+            filepath=os.path.join(self.model_dir, "{}_best_model.h5".format(current_time)),
+            save_best_only=True, monitor='val_loss', mode='min', verbose=1)
+        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=patience_es, restore_best_weights=True)
+
+        return [tensorboard_callback, reduce_lr_callback, model_checkpoint_callback, early_stopping_callback]
+
     def evaluate_model(self, test_datagen):
-        if self.model is None:
-            raise ValueError("Model has not been built. Call build_model() first.")
+        """Evaluate the model."""
+        self._check_model()
 
         test_generator = test_datagen.flow_from_directory(
             self.test_dir,
-            target_size=(150, 150),
-            batch_size=32,
+            target_size=self.img_size,
+            batch_size=self.batch_size,
             class_mode='categorical')
 
         test_loss, test_acc = self.model.evaluate(test_generator)
@@ -112,7 +129,11 @@ class SkinCancerDetector:
         return test_loss, test_acc
 
     def save_model(self, filename='models/skinvestigator_nano_40MB_91_38_acc.h5'):
+        """Save the model."""
+        self._check_model()
+        self.model.save(filename)
+
+    def _check_model(self):
+        """Check if model has been built."""
         if self.model is None:
             raise ValueError("Model has not been built. Call build_model() first.")
-
-        self.model.save(filename)
