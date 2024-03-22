be5036fd5648243dd34e89dfe94419d6dddc920e
Thomas Behan <thomasbehan4@gmail.com>
2024-03-22 00:25:11 +0000

    M-2.0.1 - 🔧⚖️ Improve class balance handling & augment data in SkinCancerDetector
    - Added class weights calculation to balance training data.
    - Integrated data augmentation directly in preprocessing.
    - Included F1 score as a metric for better performance evaluation.
    - Simplified data pipeline by embedding rescaling into the model.

--- skinvestigatorai/core/ai/detector.py
+++ skinvestigatorai/core/ai/detector.py
@@ -4,13 +4,16 @@ import tensorflow as tf
 from tensorflow.keras.metrics import Precision, Recall
 from tensorflow.keras.callbacks import TensorBoard, ReduceLROnPlateau, ModelCheckpoint, EarlyStopping
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
+from tensorflow.keras.layers import Rescaling
 from PIL import Image
+import numpy as np
 import kerastuner as kt
 
+def f1_score(precision, recall):
+    return 2*((precision*recall)/(precision+recall+tf.keras.backend.epsilon()))
 
 class SkinCancerDetector:
-    def __init__(self, train_dir, val_dir, test_dir, log_dir='logs', batch_size=32, model_dir='models',
-                 img_size=(180, 180)):
+    def __init__(self, train_dir, val_dir, test_dir, log_dir='logs', batch_size=32, model_dir='models', img_size=(180, 180)):
         self.train_dir = train_dir
         self.val_dir = val_dir
         self.test_dir = test_dir
@@ -41,12 +44,12 @@ class SkinCancerDetector:
                         print('Deleted invalid file:', img_path)  # Log out the names of deleted files
         return invalid_images
 
-    def preprocess_data(self):
+    def preprocess_data(self, augment=True):
         self.verify_images(self.train_dir)
-        self.verify_images(self.test_dir)
-        train_generator = self.create_data_generator(self.train_dir, augment=False)
-        val_generator = self.create_data_generator(self.test_dir)
-        test_datagen = self.create_data_generator(self.test_dir)
+        self.verify_images(self.val_dir)
+        train_generator = self.create_data_generator(self.train_dir, augment=augment)
+        val_generator = self.create_data_generator(self.val_dir, augment=False)
+        test_datagen = self.create_data_generator(self.test_dir, augment=False)
         return train_generator, val_generator, test_datagen
 
     def create_data_generator(self, dir=None, augment=False):
@@ -55,7 +58,13 @@ class SkinCancerDetector:
                 rescale=1. / 255,
                 horizontal_flip=True,
                 vertical_flip=True,
-                brightness_range=[0.8, 1.2]
+                rotation_range=20,
+                width_shift_range=0.2,
+                height_shift_range=0.2,
+                shear_range=0.2,
+                zoom_range=0.2,
+                brightness_range=[0.8, 1.2],
+                fill_mode='nearest'
             )
         else:
             datagen = ImageDataGenerator(rescale=1. / 255)
@@ -77,6 +86,7 @@ class SkinCancerDetector:
 
     def build_model(self, num_classes=2):
         self.model = tf.keras.Sequential([
+            Rescaling(1./255, input_shape=(self.img_size[0], self.img_size[1], 3)),
             tf.keras.layers.Conv2D(128, 3, padding='same', activation='relu'),
             tf.keras.layers.MaxPooling2D(),
             tf.keras.layers.Dropout(0.1),
@@ -89,134 +99,50 @@ class SkinCancerDetector:
             tf.keras.layers.Flatten(),
             tf.keras.layers.Dense(64, activation='relu'),
             tf.keras.layers.Dropout(0.3),
-            tf.keras.layers.Dense(96, activation='relu'),
+            tf.keras.layers.Dense(96,            activation='relu'),
             tf.keras.layers.Dropout(0.1),
-            tf.keras.layers.Dense(1, activation='softmax' if num_classes > 2 else 'sigmoid')
+            tf.keras.layers.Dense(1, activation='sigmoid')
         ])
 
         self.model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
                            loss='binary_crossentropy',
                            metrics=[
                                'accuracy',
-                               tf.keras.metrics.Precision(name='precision'),
-                               tf.keras.metrics.Recall(name='recall'),
-                               tf.keras.metrics.AUC(name='auc')
+                               Precision(name='precision'),
+                               Recall(name='recall'),
+                               tf.keras.metrics.AUC(name='auc'),
+                               tf.keras.metrics.BinaryAccuracy(name='binary_accuracy'),
+                               f1_score
                            ])
 
-    def train_model(self, train_generator, val_generator, epochs=1000, patience_lr=100, patience_es=100, min_lr=1e-6,
-                    min_delta=1e-3, cooldown_lr=20):
+    def train_model(self, train_generator, val_generator, class_weights=None, epochs=1000, patience_lr=10, patience_es=30, min_lr=1e-6, min_delta=1e-4, cooldown_lr=5):
         self._check_model()
         current_time = datetime.datetime.now().strftime("%Y%m%d-%H%M%S")
         log_dir = os.path.join(self.log_dir, current_time)
         os.makedirs(log_dir, exist_ok=True)
-        callbacks = self._create_callbacks(log_dir, current_time, patience_lr, min_lr, min_delta, patience_es,
-                                           cooldown_lr)
-        history = self.model.fit(train_generator, epochs=epochs, validation_data=val_generator, callbacks=callbacks)
+        callbacks = self._create_callbacks(log_dir, current_time, patience_lr, min_lr, min_delta, patience_es, cooldown_lr)
+        history = self.model.fit(train_generator,
+                                 epochs=epochs,
+                                 validation_data=val_generator,
+                                 class_weight=class_weights,
+                                 callbacks=callbacks)
         return history
 
-    def HParam_tuning(self, train_generator, val_generator, epochs=1000):
-        def model_builder(hp):
-            model = tf.keras.Sequential()
-            model.add(tf.keras.layers.Rescaling(1. / 255, input_shape=(self.img_size[0], self.img_size[1], 3)))
-
-            # Hyperparameters for the convolutional layers
-            for i in range(hp.Int('conv_blocks', 1, 3, default=2)):
-                hp_filters = hp.Int(f'filters_{i}', min_value=32, max_value=256, step=32)
-                model.add(
-                    tf.keras.layers.Conv2D(filters=hp_filters, kernel_size=(3, 3), activation='relu', padding='same'))
-                model.add(tf.keras.layers.MaxPooling2D(pool_size=(2, 2)))
-                model.add(tf.keras.layers.Dropout(
-                    rate=hp.Float(f'dropout_conv_{i}', min_value=0.0, max_value=0.5, default=0.25, step=0.05)))
-
-            model.add(tf.keras.layers.Flatten())
-
-            # Hyperparameters for the dense layers
-            for i in range(hp.Int('dense_blocks', 1, 2, default=1)):
-                hp_units = hp.Int(f'units_{i}', min_value=32, max_value=1028, step=32)
-                model.add(tf.keras.layers.Dense(units=hp_units, activation='relu'))
-                model.add(tf.keras.layers.Dropout(
-                    rate=hp.Float(f'dropout_dense_{i}', min_value=0.0, max_value=0.5, default=0.5, step=0.05)))
-
-            # Output layer
-            model.add(tf.keras.layers.Dense(1, activation='sigmoid'))
-
-            # Tuning the learning rate
-            hp_learning_rate = hp.Choice('learning_rate', values=[1e-2, 1e-3, 1e-4])
-
-            model.compile(optimizer=tf.keras.optimizers.Adam(learning_rate=hp_learning_rate),
-                          loss='binary_crossentropy',
-                          metrics=[
-                               'accuracy',
-                               tf.keras.metrics.Precision(name='precision'),
-                               tf.keras.metrics.Recall(name='recall'),
-                               tf.keras.metrics.AUC(name='auc')
-                           ])
-
-            return model
-
-        tuner = kt.Hyperband(model_builder,
-                             objective='val_loss',
-                             max_epochs=epochs,
-                             factor=5,
-                             directory='hyperband_logs',
-                             seed=42,
-                             hyperband_iterations=2,
-                             project_name='skin_cancer_detection')
-
-        class ClearTrainingOutput(tf.keras.callbacks.Callback):
-            def on_train_end(*args, **kwargs):
-                return
-
-        # Adding a callback for TensorBoard
-        log_dir = f"logs/hparam_tuning/{datetime.datetime.now().strftime('%Y%m%d-%H%M%S')}"
-        tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir, histogram_freq=1)
-
-        tuner.search(train_generator,
-                     epochs=epochs,
-                     validation_data=val_generator,
-                     callbacks=[ClearTrainingOutput(), tensorboard_callback])
-
-        # Get the optimal hyperparameters
-        best_hps = tuner.get_best_hyperparameters(num_trials=1)[0]
-
-        print("The hyperparameter search is complete.")
-
-        # Train the model with the best hyperparameters
-        best_model = tuner.hypermodel.build(best_hps)
-        best_model.fit(train_generator,
-                       epochs=epochs,
-                       validation_data=val_generator,
-                       callbacks=[tensorboard_callback])
-
     def _create_callbacks(self, log_dir, current_time, patience_lr, min_lr, min_delta, patience_es, cooldown_lr):
-        tensorboard_callback = TensorBoard(
-            log_dir=log_dir, histogram_freq=1, write_graph=True, write_images=True, update_freq='epoch', profile_batch=0
-        )
-        reduce_lr_callback = ReduceLROnPlateau(
-            monitor='val_loss', factor=0.2, patience=patience_lr, min_lr=min_lr, min_delta=min_delta,
-            cooldown=cooldown_lr, verbose=1
-        )
-        model_checkpoint_callback = ModelCheckpoint(
-            filepath=os.path.join(self.model_dir, f"{current_time}_best_model.h5"), save_best_only=True,
-            monitor='val_loss', mode='min', verbose=1
-        )
-        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=patience_es, restore_best_weights=True,
-                                                verbose=1)
+        tensorboard_callback = TensorBoard(log_dir=log_dir, histogram_freq=1, write_graph=True, write_images=True, update_freq='epoch', profile_batch=0)
+        reduce_lr_callback = ReduceLROnPlateau(monitor='val_loss', factor=0.2, patience=patience_lr, min_lr=min_lr, min_delta=min_delta, cooldown=cooldown_lr, verbose=1)
+        model_checkpoint_callback = ModelCheckpoint(filepath=os.path.join(self.model_dir, f"{current_time}_best_model.h5"), save_best_only=True, monitor='val_loss', mode='min', verbose=1)
+        early_stopping_callback = EarlyStopping(monitor='val_loss', patience=patience_es, restore_best_weights=True, verbose=1)
 
         return [tensorboard_callback, reduce_lr_callback, model_checkpoint_callback, early_stopping_callback]
 
     def evaluate_model(self, test_datagen):
         self._check_model()
-        test_loss, test_acc, test_precision, test_recall, test_auc = self.model.evaluate(test_datagen)
-        print(
-            f'Test accuracy: {test_acc}, '
-            f'Test precision: {test_precision}, '
-            f'Test recall: {test_recall}, '
-            f'Test AUC: {test_auc}'
-        )
-        return test_loss, test_acc, test_precision, test_recall, test_auc
+        test_loss, test_acc, test_precision, test_recall, test_auc, test_binary_accuracy, test_f1_score = self.model.evaluate(test_datagen)
+        print(f'Test accuracy: {test_acc}, Test precision: {test_precision}, Test recall: {test_recall}, Test AUC: {test_auc}, Test F1 Score: {test_f1_score}')
+        return test_loss, test_acc, test_precision, test_recall, test_auc, test_binary_accuracy, test_f1_score
 
-    def save_model(self, filename='models/skinvestigator.h5'):
+    def save_model(self, filename='models/skin_cancer_detector.h5'):
         self._check_model()
         self.model.save(filename)
         tflite_model = self.quantize_model(self.model)
@@ -226,9 +152,11 @@ class SkinCancerDetector:
         print(f"Model saved as {filename} and {tflite_model_path}")
 
     def load_model(self, filename):
-        self.model = tf.keras.models.load_model(filename, custom_objects={"Precision": Precision, "Recall": Recall})
+        self.model = tf.keras.models.load_model(filename, custom_objects={"Precision": Precision, "Recall": Recall, "f1_score": f1_score})
         print(f"Model loaded from {filename}")
 
     def _check_model(self):
         if self.model is None:
             raise ValueError("Model has not been built. Call build_model() first.")
+
+
--- skinvestigatorai/core/ai/train.py
+++ skinvestigatorai/core/ai/train.py
@@ -4,6 +4,21 @@ from skinvestigatorai.core.ai.config import train_dir, val_dir, test_dir
 from skinvestigatorai.core.data_scraper import DataScraper
 
 
+def calculate_class_weights(train_dir):
+    # Count the number of benign and malignant cases in the training set
+    num_benign = len(os.listdir(os.path.join(train_dir, 'benign')))
+    num_malignant = len(os.listdir(os.path.join(train_dir, 'malignant')))
+    total = num_benign + num_malignant
+
+    # Calculate class weights
+    weight_for_benign = (1 / num_benign) * (total / 2.0)
+    weight_for_malignant = (1 / num_malignant) * (total / 2.0)
+
+    class_weights = {0: weight_for_benign, 1: weight_for_malignant}
+
+    return class_weights
+
+
 def main(filename='models/skinvestigator.h5'):
     # check if data is downloaded and if not download it
     if not os.path.exists(train_dir):
@@ -22,16 +37,14 @@ def main(filename='models/skinvestigator.h5'):
     detector = SkinCancerDetector(train_dir, val_dir, test_dir)
     train_generator, val_generator, test_datagen = detector.preprocess_data()
     detector.build_model(num_classes=len(train_generator.class_indices))
-    detector.train_model(train_generator, val_generator)
+
+    # Calculate class weights for balanced training
+    class_weights = calculate_class_weights(train_dir)
+
+    detector.train_model(train_generator, val_generator, class_weights=class_weights)
     detector.evaluate_model(test_datagen)
     detector.save_model(filename)
 
 
 if __name__ == '__main__':
-
-    if not os.path.exists('data/train/benign'):
-        downloader = DataScraper()
-        print('Done training models')
-        print('Training model with all data')
-        downloader.download_and_split_images()
     main('skin_cancer_detection_model_all_GPU.h5')
