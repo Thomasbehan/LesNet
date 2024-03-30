f31753d49766257c4f0fd0cecb43b6b79630f677
Thomas Behan <thomasbehan4@gmail.com>
2024-03-30 21:28:58 +0000

    M-3 - :sparkles: Implement complex model architecture for enhanced skin cancer detection performance

--- commands/run_hparam_tuning.py
+++ commands/run_hparam_tuning.py
@@ -1,10 +1,20 @@
 import os
+import tensorflow as tf
 from skinvestigatorai.services.detector_service import SkinCancerDetector
 from __config import train_dir, val_dir, test_dir
 from skinvestigatorai.services.data_scaper_service import DataScraper
 
 
 def main():
+    # Ensure that GPUs are used efficiently
+    gpus = tf.config.experimental.list_physical_devices('GPU')
+    if gpus:
+        try:
+            for gpu in gpus:
+                tf.config.experimental.set_memory_growth(gpu, True)
+        except RuntimeError as e:
+            print(e)
+
     # check if data is downloaded and if not download it
     if not os.path.exists(train_dir):
         print('Downloading data...')
