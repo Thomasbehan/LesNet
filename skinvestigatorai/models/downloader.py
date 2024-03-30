8bdc2d882a40b269b78995629bdff42d5697a2f4
Thomas Behan <thomasbehan4@gmail.com>
2024-03-30 13:58:00 +0000

    M-3 - 🔧:bug: fixing small bug causing it to save as a zip file rather than a usable model

--- skinvestigatorai/models/downloader.py
+++ skinvestigatorai/models/downloader.py
@@ -1,6 +1,7 @@
 import os
 import requests
 from tqdm import tqdm
+from urllib.parse import urlparse, unquote
 
 MODEL_DIRECTORY = 'models/'
 MODEL_URLS = {
@@ -20,10 +21,13 @@ def downloader(model_name):
     response = requests.get(url, stream=True)
 
     if response.status_code == 200:
+        parsed_url = urlparse(url)
+        filename = os.path.basename(unquote(parsed_url.path))
+
         total_size_in_bytes = int(response.headers.get('content-length', 0))
         block_size = 1024  # 1 Kilobyte
         progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
-        model_path = os.path.join(MODEL_DIRECTORY, f"{model_name}")
+        model_path = os.path.join(MODEL_DIRECTORY, filename)
 
         if not os.path.exists(MODEL_DIRECTORY):
             os.makedirs(MODEL_DIRECTORY)
@@ -38,7 +42,7 @@ def downloader(model_name):
             print("ERROR, something went wrong")
             return False
 
-        print(f"Downloaded {model_name} successfully.")
+        print(f"Downloaded {model_name} successfully as {filename}.")
         return True
     else:
         print(f"Failed to download {model_name}.")
