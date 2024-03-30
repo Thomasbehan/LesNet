c9da42a7652220fb65b062a67c06c76205c9f39b
Thomas Behan <thomasbehan4@gmail.com>
2024-03-30 13:50:54 +0000

    M-3 - 🔧:sparkles: adding progress bar to model downloader

--- skinvestigatorai/models/downloader.py
+++ skinvestigatorai/models/downloader.py
@@ -1,7 +1,7 @@
 import os
 import requests
+from tqdm import tqdm
 
-# Define model locations and URLs
 MODEL_DIRECTORY = 'models/'
 MODEL_URLS = {
     'M-0003':
@@ -12,18 +12,32 @@ MODEL_URLS = {
 
 
 def downloader(model_name):
-    """Download a model from a URL into the specified directory."""
     url = MODEL_URLS.get(model_name)
     if not url:
         print(f"URL for model '{model_name}' not found.")
         return False
-    response = requests.get(url)
+
+    response = requests.get(url, stream=True)
+
     if response.status_code == 200:
+        total_size_in_bytes = int(response.headers.get('content-length', 0))
+        block_size = 1024  # 1 Kilobyte
+        progress_bar = tqdm(total=total_size_in_bytes, unit='iB', unit_scale=True)
+        model_path = os.path.join(MODEL_DIRECTORY, f"{model_name}")
+
         if not os.path.exists(MODEL_DIRECTORY):
             os.makedirs(MODEL_DIRECTORY)
-        model_path = os.path.join(MODEL_DIRECTORY, f"{model_name}.zip")
-        with open(model_path, 'wb') as f:
-            f.write(response.content)
+
+        with open(model_path, 'wb') as file:
+            for data in response.iter_content(block_size):
+                progress_bar.update(len(data))
+                file.write(data)
+        progress_bar.close()
+
+        if total_size_in_bytes != 0 and progress_bar.n != total_size_in_bytes:
+            print("ERROR, something went wrong")
+            return False
+
         print(f"Downloaded {model_name} successfully.")
         return True
     else:
