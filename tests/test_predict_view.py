efa7270ca51ccc3033639179fe807f1a6058b2e1
Thomas Behan <thomasbehan4@gmail.com>
2024-03-31 22:50:35 +0100

    M-3 - fixing test error in predict view

--- tests/test_predict_view.py
+++ tests/test_predict_view.py
@@ -4,6 +4,7 @@ from unittest.mock import MagicMock, patch
 from skinvestigatorai.views.predict_view import predict_view, dashboard_view
 from pyramid import testing
 import pytest
+import glob
 
 
 @pytest.fixture
@@ -15,17 +16,27 @@ def mock_is_image_similar():
         yield
 
 
+def find_random_jpg(data_directory):
+    # Search recursively for JPG files within the specified directory
+    jpg_files = glob.glob(os.path.join(data_directory, '**/*.jpg'), recursive=True)
+
+    # Ensure there is at least one JPG file found
+    if not jpg_files:
+        raise FileNotFoundError("No JPG files found in the directory.")
+
+    # Select a random file from the list
+    random_file = random.choice(jpg_files)
+
+    return random_file
+
+
 def test_predict_view(mock_is_image_similar):
     request = testing.DummyRequest()
 
-    benign_directory = 'data/temp/benign'
+    data_directory = 'data'
+    random_file = find_random_jpg(data_directory)
 
-    jpg_files = [f for f in os.listdir(benign_directory) if f.endswith('.JPG')]
-
-    random_file = random.choice(jpg_files)
-    file_path = os.path.join(benign_directory, random_file)
-
-    with open(file_path, 'rb') as image_file:
+    with open(random_file, 'rb') as image_file:
         dummy_file_upload = MagicMock()
         dummy_file_upload.file = image_file
         request.POST['image'] = dummy_file_upload
