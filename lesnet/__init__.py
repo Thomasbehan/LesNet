4a7fc2bb703b5421e24a92c821d96a3f027c2a16
Thomas Behan <thomas@ionadrones.com>
2026-06-10 01:10:23 +0100

    M-4 - :wrench: Lazy-import pyramid in package init so lesnet.ml works without the web stack

--- lesnet/__init__.py
+++ lesnet/__init__.py
@@ -1,13 +1,13 @@
-from pyramid.config import Configurator
-
-
 def main(global_config, **settings):
-    """ This function returns a Pyramid WSGI application.
+    """Return a Pyramid WSGI application.
+
+    Pyramid is imported lazily so the ML subpackage (lesnet.ml) can be used in
+    training/inference environments that don't install the web stack.
     """
+    from pyramid.config import Configurator
+
     with Configurator(settings=settings) as config:
         config.include('pyramid_jinja2')
         config.include('.routes')
         config.scan()
     return config.make_wsgi_app()
-
-
